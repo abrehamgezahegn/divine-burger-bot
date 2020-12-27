33eef4aa5fddacdb2c79ba8dbc2f69e92524c781
abrehamgezahegn <gezahegnabereham@gmail.com>
2020-12-28 00:21:36 +0300

    (feat) order feature started,inline keyboards added

--- index.js
+++ index.js
@@ -24,23 +24,93 @@ const sendOrderKeyboard = (msg) => {
       inline_keyboard: [
         [
           {
-            text: "whaa",
+            text: "Becon Delux (Free coke) ",
             callback_data: JSON.stringify({
-              query: "menu",
+              type: "order",
+              meal: "Becon Delux",
+              price: 205,
+              meal_id: 1,
             }),
-            url:
-              "https://www.google.com/maps/place/Divine+Burger/@8.9987837,38.7831049,17z/data=!3m1!4b1!4m5!3m4!1s0x164b859bbc283e9b:0xe2840693f0f71555!8m2!3d8.9987784!4d38.7852989",
-            // pay: true,
+          },
+        ],
+        [
+          {
+            text: "Crispy chicken special",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Crispy chicken",
+              price: 186,
+              meal_id: 2,
+            }),
+          },
+        ],
+        [
+          {
+            text: "Divine junkie",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Divine junkie",
+              price: 176,
+              meal_id: 3,
+            }),
+          },
+        ],
+        [
+          {
+            text: "Pastrami burger",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Pastrami",
+              price: 150,
+              meal_id: 4,
+            }),
+          },
+        ],
+        [
+          {
+            text: "Smoken' Spice",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Spice",
+              price: 146,
+              meal_id: 5,
+            }),
+          },
+        ],
+        [
+          {
+            text: "California style",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Cali",
+              price: 120,
+              meal_id: 6,
+            }),
+          },
+        ],
+        [
+          {
+            text: "French Fries",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "fries",
+              price: 50,
+              meal_id: 7,
+            }),
+          },
+        ],
+        [
+          {
+            text: "Coke",
+            callback_data: JSON.stringify({
+              type: "order",
+              meal: "Coke",
+              price: 16,
+              meal_id: 8,
+            }),
           },
         ],
       ],
-      // keyboard: [
-      //   [" Divine special  💸205"],
-      //   ["Junkie 💸90"],
-      //   ["Pestramininin 💸87"],
-      //   [" Chicken special  💸180"],
-      //   ["Home"],
-      // ],
     },
   });
 };
@@ -82,28 +152,7 @@ const sendGallery = (msg) => {
     {
       type: "photo",
       media:
-        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099199/Divine/photo_2020-12-27_00-07-06.jpg",
-    },
-    {
-      type: "photo",
-      media:
-        "https://drive.google.com/file/d/1N1CXO_yZG3E9WxV80Gew7skB430pDSOS/view?usp=sharing",
-    },
-    {
-      type: "photo",
-      media:
-        "https://drive.google.com/file/d/15G_1dgDqKqo_StF4GAFvLkCTJ952sPBX/view?usp=sharing",
-    },
-    {
-      type: "photo",
-      media:
-        "https://drive.google.com/file/d/1X2MNkmjFGglXBPZl9tmzIjSXwHCAPQpf/view?usp=sharing",
-    },
-
-    {
-      type: "photo",
-      media:
-        "https://drive.google.com/file/d/1ACrwd5MOCytWU_5q9XibtbYed1aI1nmR/view?usp=sharing",
+        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099378/Divine/gallery/photo_2020-12-26_23-58-20.jpg",
     },
     {
       type: "video",
@@ -113,12 +162,35 @@ const sendGallery = (msg) => {
     {
       type: "photo",
       media:
-        "https://drive.google.com/file/d/1rO__cjh5Ng9HAjtIZu3lgxm2X-zq9IBX/view?usp=sharing",
+        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099377/Divine/gallery/photo_2020-12-27_00-02-19.jpg",
     },
     {
       type: "photo",
       media:
-        "https://drive.google.com/file/d/1iw9IGEwHgKQvuT30iJR5aqEulxmdz-TW/view?usp=sharing",
+        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099376/Divine/gallery/photo_2020-12-27_00-03-11.jpg",
+    },
+  ]);
+
+  bot.sendMediaGroup(msg.chat.id, [
+    {
+      type: "photo",
+      media:
+        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099199/Divine/photo_2020-12-27_00-07-06.jpg",
+    },
+    {
+      type: "photo",
+      media:
+        "https://res.cloudinary.com/de5awe7fs/video/upload/v1609099386/Divine/gallery/IMG_4182.mp4",
+    },
+    {
+      type: "photo",
+      media:
+        "https://drive.google.com/file/d/15G_1dgDqKqo_StF4GAFvLkCTJ952sPBX/view?usp=sharing",
+    },
+    {
+      type: "photo",
+      media:
+        "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099380/Divine/gallery/photo_2020-12-26_23-58-15.jpg",
     },
   ]);
 };
@@ -128,7 +200,6 @@ bot.onText(/\/start/, (msg) => {
 });
 
 bot.on("message", (msg) => {
-  console.log("msg", msg);
   if (msg.text === "📖 Menu") {
     sendMenuPicture(msg);
   }
@@ -151,6 +222,56 @@ bot.on("message", (msg) => {
   }
   if (msg.text === "Home") {
     sendHomeMenuKeyboard(msg);
+  }
+});
+
+bot.on("callback_query", (query) => {
+  const data = JSON.parse(query.data);
+  const chat = query.message.chat;
+  console.log("query ", query);
+
+  switch (data.type) {
+    case "order": {
+      if (data.meal_id === 1) {
+        bot.sendPhoto(
+          chat.id,
+          "https://res.cloudinary.com/de5awe7fs/image/upload/v1609099380/Divine/gallery/photo_2020-12-26_23-58-15.jpg"
+        );
+        bot.sendMessage(
+          chat.id,
+          "🍔 Becon Delux \n💰 205Birr \n👨‍🍳 Double patty , double sliced cheese , double bacon with American cheese,  double  sliced beef. -with a free coca cola \n ",
+          {
+            reply_markup: {
+              inline_keyboard: [
+                [
+                  {
+                    text: "Confirm",
+                    callback_data: JSON.stringify({
+                      type: "confirm_order",
+                      order: data.meal_id,
+                      userName: chat.username,
+                    }),
+                  },
+                  {
+                    text: "Changed my mind",
+                    callback_data: JSON.stringify({
+                      type: "show_order_menu",
+                    }),
+                  },
+                ],
+              ],
+            },
+          }
+        );
+      }
+      break;
+    }
+    case "confirm_order":
+      bot.sendMessage(chat.id, `One ${data.order} coming your way.`);
+      break;
+    case "show_order_menu":
+      sendOrderKeyboard(query.message);
+      break;
   }
 });
 
